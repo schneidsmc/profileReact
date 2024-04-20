97fdb611f95d4422976b0a8e71fb0d77b86f46e2
snipes <schneidsmc@gmail.com>
2024-04-20 15:14:07 -0500

    caught up to old repo

---
+++ profileReact/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Navbar from './components/navbar';
+import Home from './components/home';
+
+
+function App() {
+ 
+
+  return (
+    <>
+      {/* create components */}
+
+      <Navbar/>
+      <Home/>
+
+    </>
+  )
+}
+
+export default App
